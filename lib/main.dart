a6109d358a11fe0007973370eadb346270f01f14
syafiqoarashy <101589777+syafiqoarashy@users.noreply.github.com>
2022-11-17 10:43:35 +0700

    lab 8 completion

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:pbp_flutter/form.dart';
+import 'package:pbp_flutter/page/form.dart';
+import 'package:pbp_flutter/page/to_do_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -80,6 +81,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 );
               },
             ),
+            ListTile(
+    title: const Text('To Do'),
+    onTap: () {
+        // Route the menu to the to do page
+        Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const ToDoPage()),
+        );
+    },
+),
           ],
         ),
       ),
